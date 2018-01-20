533a288a8df27fc0f515dcb9f262c968ae123021
manishsharmaahd <31032226+manishsharmaahd@users.noreply.github.com>
2018-01-20 19:51:37 +0530

    Update pushscript.sh

--- pushscript.sh
+++ pushscript.sh
@@ -1,4 +1,4 @@
 export PATH=/root/.local/bin:$PATH
 eval $(aws ecr get-login | sed 's|https://||')
-docker tag ffdc:$1 019066669311.dkr.ecr.ap-southeast-1.amazonaws.com/ffdc:$1
-docker push 019066669311.dkr.ecr.ap-southeast-1.amazonaws.com/ffdc:$1
+docker tag finefeather:latest 272461118117.dkr.ecr.ap-south1.amazonaws.com/finefeather:$1
+docker push 272461118117.dkr.ecr.ap-south-1.amazonaws.com/finefeather:$1
