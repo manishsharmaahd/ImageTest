15b5f0970963b90bbffd22ecec7f01c057a6281c
manishsharmaahd <31032226+manishsharmaahd@users.noreply.github.com>
2018-03-09 19:36:45 +0530

    Update pushscript.sh

--- pushscript.sh
+++ pushscript.sh
@@ -1,4 +1,4 @@
 export PATH=/root/.local/bin:$PATH
-eval $(aws ecr get-login | sed 's|https://||')
+ eval $(aws ecr get-login --no-include-email | sed 's|https://||')
 docker tag finefeather:latest 272461118117.dkr.ecr.ap-south-1.amazonaws.com/finefeather:$1
 docker push 272461118117.dkr.ecr.ap-south-1.amazonaws.com/finefeather:$1
